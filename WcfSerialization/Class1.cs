a61c61c004741162d23631181e0a3f1ace0c42ab
Patrick Desjardins <mrdesjardins@gmail.com>
2015-03-24 12:43:25 -0700

    Show what can be Serialized and not with DataContract and DataMember
    Show what can be Serialized and not with DataContract and DataMember

---
+++ WcfSerialization/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfSerialization
+{
+    using System.Runtime.Serialization;
+
+    public class MyClass1
+    {
+        public int MyProperty1 { get; set; }
+        public string MyProperty2 { get; set; }
+    }
+    [DataContract]
+    public class MyClass2
+    {
+        public int MyProperty1 { get; set; }
+        public string MyProperty2 { get; set; }
+    }
+
+    [DataContract]
+    public class MyClass3
+    {
+        [DataMember]
+        public int MyProperty1 { get; set; }
+        [DataMember]
+        public string MyProperty2 { get; set; }
+    }
+
+
+    [DataContract]
+    public class MyClass4
+    {
+        [DataMember]
+        public int MyProperty1 { get; set; }
+        [IgnoreDataMember]
+        public string MyProperty2 { get; set; }
+    }
+
+    [DataContract]
+    public class MyClass5
+    {
+        [DataMember]
+        public int MyProperty1 { get; set; }
+
+        public string MyProperty2 { get; set; }
+    }
+}
---
+++ WcfTests/UnitTest1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WcfTests
+{
+    using System.IO;
+    using System.Runtime.Serialization;
+    using System.Text;
+    using System.Xml.Serialization;
+    using WcfSerialization;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        public static string ContractObjectToXml<T>(T obj)
+        {
+            var dataContractSerializer = new DataContractSerializer(obj.GetType());
+
+            String text;
+            using (var memoryStream = new MemoryStream())
+            {
+                dataContractSerializer.WriteObject(memoryStream, obj);
+                byte[] data = new byte[memoryStream.Length];
+                Array.Copy(memoryStream.GetBuffer(), data, data.Length);
+
+                text = Encoding.UTF8.GetString(data);
+            }
+            return text;
+        }
+
+        [TestMethod]
+        public void GivenAClassWithoutDataContract_WhenPropertyDefined_ThenSerializedByDefault()
+        {
+            // Arrange
+            var x = new MyClass1() {MyProperty1 = 98765, MyProperty2 = "asdasdasd"};
+
+            // Act
+            var stringClass = ContractObjectToXml(x);
+
+            // Assert
+            Assert.IsTrue(stringClass.Contains("98765"));
+            Assert.IsTrue(stringClass.Contains("asdasdasd"));
+
+        }
+
+        [TestMethod]
+        public void GivenAClassWithDataContract_WhenPropertyDefined_ThenNothingIsSerialized()
+        {
+            // Arrange
+            var x = new MyClass2() { MyProperty1 = 98765, MyProperty2 = "asdasdasd" };
+
+            // Act
+            var stringClass = ContractObjectToXml(x);
+
+            // Assert
+            Assert.IsFalse(stringClass.Contains("98765"));
+            Assert.IsFalse(stringClass.Contains("asdasdasd"));
+
+        }
+
+        [TestMethod]
+        public void GivenAClassWithDataContractAndDataMember_WhenPropertyDefined_ThenSerializedByDefault()
+        {
+            // Arrange
+            var x = new MyClass3() { MyProperty1 = 98765, MyProperty2 = "asdasdasd" };
+
+            // Act
+            var stringClass = ContractObjectToXml(x);
+
+            // Assert
+            Assert.IsTrue(stringClass.Contains("98765"));
+            Assert.IsTrue(stringClass.Contains("asdasdasd"));
+
+        }
+
+        [TestMethod]
+        public void GivenAClassWithDataContractAndDataMember_WhenProperty2Ignored_ThenSerializedAllButNotIgnored()
+        {
+            // Arrange
+            var x = new MyClass4() { MyProperty1 = 98765, MyProperty2 = "asdasdasd" };
+
+            // Act
+            var stringClass = ContractObjectToXml(x);
+
+            // Assert
+            Assert.IsTrue(stringClass.Contains("98765"));
+            Assert.IsFalse(stringClass.Contains("asdasdasd"));
+
+        }
+        [TestMethod]
+        public void GivenAClassWithDataContractAndDataMember_WhenProperty2NoAttribute_ThenSerializedAllButNotNoAttribute()
+        {
+            // Arrange
+            var x = new MyClass5() { MyProperty1 = 98765, MyProperty2 = "asdasdasd" };
+
+            // Act
+            var stringClass = ContractObjectToXml(x);
+
+            // Assert
+            Assert.IsTrue(stringClass.Contains("98765"));
+            Assert.IsFalse(stringClass.Contains("asdasdasd"));
+
+        }
+    }
+}
